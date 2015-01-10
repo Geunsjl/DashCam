2223111c7e3bc27ca714628bbc8ef1a1d56c6a24
Jan-Lennert <geunsjl@gmail.com>
2015-01-10 14:59:20 +0100

    Klaar voor presentatie

--- app/src/main/java/com/geunsjl/dashcam/MapsActivity.java
+++ app/src/main/java/com/geunsjl/dashcam/MapsActivity.java
@@ -1,7 +1,5 @@
 package com.geunsjl.dashcam;
 
-import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
@@ -9,8 +7,6 @@ import android.support.v4.app.FragmentActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -29,9 +25,6 @@ public class MapsActivity extends FragmentActivity {
     //DBAdapter myDb;
     DatabaseHandler myDb;
 
-    //Update camera bool
-    boolean updateCam = true;
-
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     private ArrayList<LatLng> arrayPoints = new ArrayList<LatLng>();
@@ -48,18 +41,6 @@ public class MapsActivity extends FragmentActivity {
         mMap.setMyLocationEnabled(true);
         arrayPoints.clear();
 
-        lv = (ListView) findViewById(R.id.listView);
-        arrayAdapter = new ArrayAdapter<LatLng>(
-                this, android.R.layout.simple_list_item_1, arrayPoints);
-
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        if(intent.getStringExtra("showRoute").equals("showRoute")){
-            showRoute();
-        }
     }
 
     @Override
@@ -94,8 +75,9 @@ public class MapsActivity extends FragmentActivity {
             String mydate = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
             LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
 
-            //Update camera
-            MoveCamera(loc);
+            //loc in arraylist
+            arrayPoints.add(loc);
+
             //Write location to database
             myDb.addLocation(loc.latitude, loc.longitude);
 
@@ -103,60 +85,6 @@ public class MapsActivity extends FragmentActivity {
             drawRouteOnMap();
         }
     };
-
-    public void sampleData()
-    {
-        LatLng loc2 = new LatLng(53.558, 9.927);
-        LatLng loc3 = new LatLng(53.551, 9.993);
-        //arrayPoints.add(loc);
-        arrayPoints.add(loc2);
-        arrayPoints.add(loc3);
-
-        myDb.addLocation(loc2.latitude, loc2.longitude);
-        myDb.addLocation(loc3.latitude, loc3.longitude);
-    }
-
-    //Update camera angle
-    public void MoveCamera(LatLng loc)
-    {
-        if(updateCam == true) {
-            //update map position
-            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(loc, 15);
-            mMap.animateCamera(cameraUpdate);
-        }
-    }
-
-    //display route
-    public void showRoute()
-    {   sampleData();
-        drawRouteOnMap();
-        //Cursor cursor = myDb.getAllLocations();
-        //displayRecordSet(cursor);
-    }
-
-    private void displayRecordSet(Cursor cursor) {
-        arrayPoints.clear();
-        updateCam = false;
-        if(cursor.moveToFirst())
-
-            //TODO: lees de eerste plaats en ga hiernaartoe met de camera
-            //Process data
-            do {
-                //extract fro mdb
-                double latitude = cursor.getDouble((int) DatabaseHandler.COL_LATITUTDE);
-                double longtitude = cursor.getDouble((int) DatabaseHandler.COL_LONGTITUDE);
-                System.out.print(latitude);
-                System.out.print(longtitude);
-                LatLng loc = new LatLng(latitude, longtitude);
-
-                arrayPoints.add(loc);
-
-                //draw route on map
-                drawRouteOnMap();
-            } while (cursor.moveToNext());
-        lv.setAdapter(arrayAdapter);
-        cursor.close();
-    }
 
     private void drawRouteOnMap()
     {
